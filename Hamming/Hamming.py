9d1f9f1847c1ce2435d1b8506a197e993b63188d
Curiv <>
2020-04-22 02:21:24 +0500

    Coding a encoding bit

--- Hamming/Hamming.py
+++ Hamming/Hamming.py
@@ -1,3 +1,4 @@
+from  math  import log
 
 def HammingEncode(information):
     # information - должно быть в бинарном виде
@@ -27,27 +28,36 @@ def HammingEncode(information):
         # Если позиция бита является степенью двойки, то записываем бит чётности
         if i+1 in two_in_power:
             msg.append('0')
+
+        # иначе пишем информационный бит
         else:
-            # иначе пишем информационный бит
             msg.append(information.pop(0))
 
     print('Избыточное сообщение:',msg)
-    
+
+#   N = 2  [ 1*N - 1 + i for i in range(2**(N-1)) ]
+#   N = 4  [ 3*N - 1 + i for i in range(2**(N-2)) ]
+#   N = 8  [ 3*N - 1 + i for i in range(2**(N-5)) ] 
+#   N = 16 [ 1*N - 1 + i for i in range(2**(N-12))]
+
     # performing N throught N counting
+    for N in two_in_power:
+        print(N)
 
-    N = 2
-    pos = N - 1
-    while True:
-        try:
-            msg[pos]
-            print(pos)
-            pos += 2*N
-        except IndexError:
-            break
+        if N == 1:
+            print([N - 1, 2*N, 4*N, 6*N, 8*N, 10*N])
 
-    #for pos in (0,2,4,6):
-        #counter += int(msg[pos])
-    #print("Not Even" if counter % 2 else "Even")
+        elif N == 2:
+            print([ 1*N - 1 + i for i in range(2**(N - log(N, 2))) ])
+
+        elif N == 4:
+            print([ 1*N - 1 + i for i in range(2**(N-2)) ])
+
+        elif N == 8:
+            print([ 1*N - 1 + i for i in range(2**(N-5)) ] )
+
+        elif N == 16:
+            print([ 1*N - 1 + i for i in range(2**(N-12)) ] )
 
 if __name__ == "__main__":
     info = '1101'
