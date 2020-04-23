fde353281cab16f0bb1f80db6fd23807b5b55f1d
Curiv <>
2020-04-24 00:01:31 +0500

    Same path lenght cheking. Dead end checking

--- Deikstra/Deikstra.py
+++ Deikstra/Deikstra.py
@@ -1,6 +1,7 @@
 
 def Deikstra(matrix):
     inf = 7777
+
     # Относительно какого узла считаем
     start_node = 0
 
@@ -21,7 +22,8 @@ def Deikstra(matrix):
 
     # Находим минимальную длину пути
     shortest_path = min(initial_node)
-
+    
+    #TODO Loop here maybe
     # Находим индексы узлов с одинаковой стоимостью пути
     dummy_node = initial_node
     for  i in range(len(initial_node)):
@@ -35,17 +37,26 @@ def Deikstra(matrix):
     if len(possible_nodes) > 1:
         print(f"Существует {len(possible_nodes)} узла ({possible_nodes}) с минимальной целой перемещения {shortest_path} ")
 
-    print(matrix[3])
     # Проходим по доступным узлам
     print(matrix)
+    dummy_node = possible_nodes
     for node in possible_nodes:
-        print(node)
-        if matrix[node - 1].count(inf) == 4:
+        print("Текущий индекс узла", node)
+        if matrix[node].count(inf) == 4:
             print(f"{node} узел - тупик! ")
+            visited_index = dummy_node.index(node)
+            dummy_node.pop(visited_index)
+            marks[node] = shortest_path
+    marks[dummy_node[-1]-1] = shortest_path
+    possible_nodes = dummy_node
+    print("Нетупиковые узлы", possible_nodes)
+    print("Метки", marks)
 
+        
+    possible_nodes=dummy_node
 if __name__ == "__main__":
     inf =  7777 
-    matrix = [[inf, 10, 30, 50, 10], [inf, inf, inf, inf], [inf,inf,inf,inf,10], [10, inf, 10, 30, inf]]
+    matrix = [[inf, 10, 30, 50, 10], [inf, inf, inf, inf], [inf,inf,inf,inf,10], [inf, 40, 20, inf, inf ],[10, inf, 10, 30, inf]]
     print(matrix)
 
     Deikstra(matrix)
